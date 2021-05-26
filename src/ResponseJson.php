7687faf3819dbaecf6b372b328dc369aa7c20c2f
akuikialie <aku.4l13@gmail.com>
2021-05-26 13:04:13 +0700

    update package

---
+++ src/ResponseJson.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Akuikialie\Http;
+
+/**
+ * Class ResponseJson
+ * @package Akuikialie\Http
+ */
+class ResponseJson
+{
+    public function __construct()
+    {
+    }
+
+    public function render()
+    {
+        var_dump('render-json');
+    }
+}
---
+++ src/ResponseXml.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Akuikialie\Http;
+
+/**
+ * Class ResponseXml
+ * @package Akuikialie\Http
+ */
+class ResponseXml
+{
+    public function __construct()
+    {
+    }
+
+    public function render()
+    {
+        var_dump('render-xml');
+    }
+}
