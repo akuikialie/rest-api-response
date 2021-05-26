d856a3b7ea8dd2347ce1d3bc09caf677b04970ec
akuikialie <aku.4l13@gmail.com>
2021-05-26 12:46:02 +0700

    initial

---
+++ src/HttpResponse.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Akuikialie\HttpResponse;
+
+/**
+ * Class HttpResponse
+ * @package Akuikialie\HttpResponse
+ */
+class HttpResponse
+{
+    public function __construct()
+    {
+    }
+
+    public function render()
+    {
+        var_dump('render');
+    }
+}
